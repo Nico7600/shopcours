fc81018f874b8d51292ece963768a288b4aa44b2
root <31188341+AurelienADVANCED@users.noreply.github.com>
2025-02-24 17:40:20 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,9 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 try {
     $dsn = sprintf(
         'mysql:host=%s;dbname=%s;charset=utf8mb4',
